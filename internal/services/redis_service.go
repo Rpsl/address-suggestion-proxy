06475e469b31b0b9c3ca687cdf9dcdc4f3e35b8b
Rpsl <gmail@rpsl.info>
2023-10-07 15:12:18 +0300

    Add support for Redis Sentinel connection

---
+++ internal/services/redis_service.go
@@ -0,0 +1,35 @@
+package services
+
+import (
+	"address-suggesstion-proxy/config"
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	log "github.com/sirupsen/logrus"
+)
+
+func NewRedisClient(cfg *config.Config) *redis.Client {
+	addr := fmt.Sprintf("%s:%s", cfg.RedisHost, cfg.RedisPort)
+
+	if cfg.RedisSentinel {
+		log.Debugln(fmt.Sprintf("%s", "using redis-sentinel"))
+
+		sentinel := redis.NewSentinelClient(&redis.Options{
+			Addr: fmt.Sprintf("%s:%s", cfg.RedisSentinelHost, cfg.RedisSentinelPort),
+		})
+
+		masters, err := sentinel.GetMasterAddrByName(context.Background(), cfg.RedisSentinelName).Result()
+
+		if err != nil {
+			log.WithError(err).Fatal("failed to get redis sentinel master host")
+		}
+		addr = fmt.Sprintf("%s:%s", masters[0], masters[1])
+
+		log.Debugf(fmt.Sprintf("redis master from sentinel is: %s", addr))
+	}
+	return redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: cfg.RedisAuth,
+		DB:       cfg.RedisDB,
+	})
+}
